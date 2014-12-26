73c21f40278873418fc998111a885603067bf67e
xcv58 <chenyihonglove@gmail.com>
2014-12-26 18:04:39 -0500

    update 404 logic. Fetch list from Dropbox public link.

--- assets/js/redirect-cn-xcv58-com.js
+++ assets/js/redirect-cn-xcv58-com.js
@@ -1,21 +1,13 @@
 var regexForMainURL = /.*\/\/.+?\//;
 var currentURL = document.URL;
-var newURL = currentURL.replace(regexForMainURL, "http://cn.xcv58.com/");
-var greeting = "<h1>You may visit link from my Chinese Blog</br>redirect to in 3 seconds:</br>";
-function update() {
-    document.getElementById("cn-xcv58-com").innerHTML = "<h1>You may visit link from <a href='" + newURL + "'>my Chinese Blog</a> redirect to it in <font color='red'>" + timer / 1000 + "</font> seconds</h1>";
+var newURL = currentURL.replace(regexForMainURL, "https://cn.xcv58.com/");
+var checkAddress = "https://dl.dropboxusercontent.com/u/4121165/cn-blog-list/list"
+
+var xmlHttp = new XMLHttpRequest();
+xmlHttp.open( "GET", checkAddress, false );
+xmlHttp.send( null );
+var response = xmlHttp.responseText;
+
+if (response.indexOf(newURL.replace(/.*cn.xcv58.com/, "")) != -1) {
+    window.location = newURL;
 }
-// var xmlHttp = new XMLHttpRequest();
-// xmlHttp.open( "GET", newURL );
-// xmlHttp.send( null );
-// var response = xmlHttp.responseText;
-var timer = 5000;
-update()
-setInterval(function () {
-    timer -= 1000;
-    if (timer == 0) {
-        window.location = newURL;
-    } else {
-        update();
-    }
-}, 1000);
