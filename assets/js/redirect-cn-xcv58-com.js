378e0d9b488001068fd4b1cb8fb04952d3587860
xcv58 <chenyihonglove@gmail.com>
2015-01-01 12:27:14 -0500

    Integrate short link redirect from https://github.com/xcv58/xcv58.me

--- assets/js/redirect-cn-xcv58-com.js
+++ assets/js/redirect-cn-xcv58-com.js
@@ -1,13 +1,20 @@
 var regexForMainURL = /.*\/\/.+?\//;
 var currentURL = document.URL;
 var newURL = currentURL.replace(regexForMainURL, "http://cn.xcv58.com/").replace(/\?.*/, '');
-var checkAddress = "https://dl.dropboxusercontent.com/u/4121165/cn-blog-list/list"
+var suffix = currentURL.replace(regexForMainURL, "").replace(/\?.*/, '');
+var cnBlogListAddress = "https://dl.dropboxusercontent.com/u/4121165/cn-blog-list/list";
 
 var xmlHttp = new XMLHttpRequest();
-xmlHttp.open( "GET", checkAddress, false );
+xmlHttp.open( "GET", cnBlogListAddress, false );
 xmlHttp.send( null );
 var response = xmlHttp.responseText;
-
 if (response.indexOf(newURL.replace(/.*cn.xcv58.com/, "")) != -1) {
     window.location = newURL;
+}
+
+var dropboxPublicURL = "https://dl.dropboxusercontent.com/u/4121165/xcv58.me/" + suffix + ".txt";
+xmlHttp.open( "GET", dropboxPublicURL, false );
+xmlHttp.send( null );
+if (xmlHttp.status == 200) {
+    window.location = xmlHttp.responseText;
 }
